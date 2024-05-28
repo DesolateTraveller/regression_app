2b7afaad81705f91097cb9064fd72fbea39b53a4
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-05-28 11:45:02 +0530

    Update ml_reg_app.py

--- ml_reg_app.py
+++ ml_reg_app.py
@@ -80,13 +80,12 @@ from sklearn.cluster import KMeans
 ### Title and description for your Streamlit app
 #---------------------------------------------------------------------------------------------------------------------------------
 #import custom_style()
-#image = Image.open('Image_Clariant.png')
-st.set_page_config(page_title="Regression App | v1.0",
+st.set_page_config(page_title="Regression App",
                    layout="wide",
                    #page_icon=               
                    initial_sidebar_state="collapsed")
 #----------------------------------------
-st.title("ML | Regression App | v1.0")
+st.title(f""":rainbow[ML | Regression App | v1.0]""")
 st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
             unsafe_allow_html=True)
 st.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
